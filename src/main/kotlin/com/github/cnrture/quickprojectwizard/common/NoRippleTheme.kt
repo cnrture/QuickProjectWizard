993e74ccda38ffe28e3506c5e8de8b5e628e8c6e
candroid <cnrture@gmail.com>
2025-06-12 02:32:00 +0300

    Add NoRippleInteractionSource and update QPWTabRow for improved interaction handling

---
+++ src/main/kotlin/com/github/cnrture/quickprojectwizard/common/NoRippleTheme.kt
@@ -0,0 +1,12 @@
+package com.github.cnrture.quickprojectwizard.common
+
+import androidx.compose.foundation.interaction.Interaction
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+
+class NoRippleInteractionSource : MutableInteractionSource {
+    override val interactions: Flow<Interaction> = emptyFlow()
+    override suspend fun emit(interaction: Interaction) {}
+    override fun tryEmit(interaction: Interaction) = true
+}
--- src/main/kotlin/com/github/cnrture/quickprojectwizard/components/QPWTabRow.kt
+++ src/main/kotlin/com/github/cnrture/quickprojectwizard/components/QPWTabRow.kt
@@ -2,32 +2,45 @@ package com.github.cnrture.quickprojectwizard.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Tab
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.github.cnrture.quickprojectwizard.common.NoRippleInteractionSource
 import com.github.cnrture.quickprojectwizard.theme.QPWTheme
 
 @Composable
 fun QPWTabRow(text: String, color: Color, isSelected: Boolean, onTabSelected: () -> Unit) {
     Box(
-        modifier = Modifier.then(
-            if (isSelected) {
-                Modifier.background(
-                    brush = Brush.verticalGradient(listOf(QPWTheme.colors.black, color.copy(alpha = 0.3f)))
-                )
-            } else {
-                Modifier.background(QPWTheme.colors.black)
-            }
-        )
+        modifier = Modifier
+            .then(
+                if (isSelected) {
+                    Modifier.background(
+                        brush = Brush.verticalGradient(listOf(QPWTheme.colors.black, color.copy(alpha = 0.3f)))
+                    )
+                } else {
+                    Modifier.background(QPWTheme.colors.black)
+                }
+            )
     ) {
         Tab(
+            modifier = Modifier.padding(8.dp),
             selected = isSelected,
             onClick = onTabSelected,
+            interactionSource = NoRippleInteractionSource(),
             text = {
                 QPWText(
                     text = text,
+                    style = TextStyle(
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                    ),
                     color = QPWTheme.colors.white,
                 )
             }
