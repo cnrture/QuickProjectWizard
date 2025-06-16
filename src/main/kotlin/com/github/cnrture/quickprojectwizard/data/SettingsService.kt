261df8d337357e1e98f8995f12104a37b7cc6e60
candroid <cnrture@gmail.com>
2025-06-16 16:10:27 +0300

    Add UI components and ViewModel templates for new screen structure

--- src/main/kotlin/com/github/cnrture/quickprojectwizard/data/SettingsService.kt
+++ src/main/kotlin/com/github/cnrture/quickprojectwizard/data/SettingsService.kt
@@ -146,9 +146,156 @@ fun getDefaultModuleTemplates(): List<ModuleTemplate> {
             name = "Candroid's Module",
             fileTemplates = listOf(
                 FileTemplate(
-                    fileName = "Repository.kt",
-                    filePath = "domain/repository",
-                    fileContent = "interface {NAME}Repository {\n    // Define methods here\n}",
+                    fileName = "{NAME}Screen.kt",
+                    filePath = "ui",
+                    fileContent = """
+package {FILE_PACKAGE}
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.sp
+import {PACKAGE}.{NAME}Contract.UiState
+import {PACKAGE}.{NAME}Contract.UiEffect
+import {PACKAGE}.{NAME}Contract.UiAction
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+
+@Composable
+fun {NAME}Screen(
+    uiState: UiState,
+    uiEffect: Flow<UiEffect>,
+    onAction: (UiAction) -> Unit
+) {
+    {NAME}Content(
+        modifier = Modifier.fillMaxSize(),
+        uiState = uiState,
+        onAction = onAction,
+    )
+}
+
+@Composable
+private fun {NAME}Content(
+    modifier: Modifier = Modifier,
+    uiState: UiState,
+    onAction: (UiAction) -> Unit,
+) {
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(
+            text = "{NAME} Content",
+            fontSize = 24.sp,
+        )
+    }
+}
+        
+@Preview(showBackground = true)
+@Composable
+fun {NAME}ScreenPreview(
+    @PreviewParameter({NAME}ScreenPreviewProvider::class) uiState: UiState,
+) {
+    {NAME}Screen(
+        uiState = uiState,
+        uiEffect = emptyFlow(),
+        onAction = {},
+    )
+}
+""".trimIndent(),
+                ),
+                FileTemplate(
+                    fileName = "{NAME}ViewModel.kt",
+                    filePath = "ui",
+                    fileContent = """
+package {FILE_PACKAGE}
+
+import androidx.lifecycle.ViewModel
+import {PACKAGE}.{NAME}Contract.UiState
+import {PACKAGE}.{NAME}Contract.UiEffect
+import {PACKAGE}.{NAME}Contract.UiAction
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+
+@HiltViewModel
+class {NAME}ViewModel @Inject constructor() : ViewModel() {
+
+    private val _uiState = MutableStateFlow(UiState())
+    val uiState: StateFlow<UiState> = _uiState.asStateFlow()
+
+    private val _uiEffect by lazy { Channel<UiEffect>() }
+    val uiEffect: Flow<UiEffect> by lazy { _uiEffect.receiveAsFlow() }
+
+    fun onAction(uiAction: UiAction) {
+    }
+
+    private fun updateUiState(block: UiState.() -> UiState) {
+        _uiState.update(block)
+    }
+
+    private suspend fun emitUiEffect(uiEffect: UiEffect) {
+        _uiEffect.send(uiEffect)
+    }
+}
+""".trimIndent(),
+                ),
+                FileTemplate(
+                    fileName = "{NAME}Contract.kt",
+                    filePath = "ui",
+                    fileContent = """
+package {FILE_PACKAGE}
+
+object {NAME}Contract {
+    data class UiState(
+        val isLoading: Boolean = false,
+        val list: List<String> = emptyList(),
+    )
+
+    sealed interface UiAction
+            
+    sealed interface UiEffect
+}
+""".trimIndent(),
+                ),
+                FileTemplate(
+                    fileName = "{NAME}ComponentKey.kt",
+                    filePath = "ui",
+                    fileContent = """
+package {FILE_PACKAGE}
+                       
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+
+class {NAME}ScreenPreviewProvider : PreviewParameterProvider<{NAME}Contract.UiState> {
+    override val values: Sequence<{NAME}Contract.UiState>
+        get() = sequenceOf(
+            {NAME}Contract.UiState(
+                isLoading = true,
+                list = emptyList(),
+            ),
+            {NAME}Contract.UiState(
+                isLoading = false,
+                list = emptyList(),
+            ),
+            {NAME}Contract.UiState(
+                isLoading = false,
+                list = listOf("Item 1", "Item 2", "Item 3")
+            ),
+        )
+}
+""".trimIndent(),
                 ),
             ),
             isDefault = true,
